6b2edc49d03de46eb9ab3e91d34118ca1be77ad1
Paweł Powroźnik <pawel.powroznik21@gmail.com>
2020-01-20 17:40:38 +0000

    Automatically backed up by Learn

--- js/script.js
+++ js/script.js
@@ -1,2 +1,2 @@
-type="text/javascipr" src="http://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js";
+src="http://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js"
 $("#text").append("this is so freaking cool.");
